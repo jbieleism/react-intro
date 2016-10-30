226df7c5961c37bb70d12a4228d39640a5286015
Justin Biele <justin.biele@gmail.com>
2016-10-30 16:51:13 -0700

    [add] Add video list with individual entries

---
+++ src/components/video-list-entry.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const VideoListEntry = (props) => {
+
+  return <li>Video</li>
+
+}
+
+
+export default VideoListEntry
--- src/components/video-list.js
+++ src/components/video-list.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import VideoListEntry from './video-list-entry'
+
 
 const VideoList = (props) => {
 
+  const videoList = props.videos.map((video) => {
+    return <VideoListEntry video={ video: video }/>
+  })
+
   return(
     <ul className="col-md-4 list-group">
-      {props.videos.length}
+      {videoList}
     </ul>
   )
 
